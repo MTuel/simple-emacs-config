4e31f7b49e961158d5323ecaf3ab819b223e2768
ItsLolgical <signupthing@itslolgical.com>
2023-10-22 22:28:15 -0500

    Updated to fix issues with launching under daemon.

--- init.el
+++ init.el
@@ -41,9 +41,19 @@
 
 ;; If the font is installed then we will use it, otherwise it should just skip.
 ;; condition-case operates like a try/catch in other languages.
-(condition-case nil
-    (set-frame-font "CaskaydiaCove NF 10" nil t)
-  (error nil))
+(defun my/configure-custom-font ()
+  "Set the font if it exists, otherwise ignore."
+  (condition-case nil
+    (setq default-frame-alist '((font . "CaskaydiaCove NF 10")))
+    (error nil)))
+
+(my/configure-custom-font)
+
+(if (daemonp)
+  (add-hook 'after-make-frame-functions
+    (lambda (frame)
+      (with-selected-frame frame
+	(my/configure-custom-font)))))
 
 ;; Set line numbers to display.
 ;; https://www.emacswiki.org/emacs/LineNumbers
