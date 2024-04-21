7013d69eb161326f822dc92af1155ae031f7354a
ItsLolgical <signupthing@itslolgical.com>
2024-04-21 17:50:19 -0500

    Updated default colors in early-init.

--- early-init.el
+++ early-init.el
@@ -4,7 +4,7 @@
 (setq native-comp-async-report-warnings-errors 'silent)
 
 ;; Set the background color to match Gruvbox and prevent the typical flashbang to the eyes at startup.
-(add-to-list 'default-frame-alist '(foreground-color . "#EBDBB2"))
-(add-to-list 'default-frame-alist '(background-color . "#282828"))
+(add-to-list 'default-frame-alist '(foreground-color . "#727780"))
+(add-to-list 'default-frame-alist '(background-color . "#22262d"))
 
 (setq load-prefer-newer noninteractive)
